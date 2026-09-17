761833a04595d6c9222daf77e6503ccaa05b2778
klckylmz-art <klckylmz@gmail.com>
2026-09-17 16:37:46 +0300

    test: target manual input highlight injection

--- puantaj-desktop/tests/manual-highlight.test.js
+++ puantaj-desktop/tests/manual-highlight.test.js
@@ -3,9 +3,9 @@ const assert=require('node:assert/strict');
 const fs=require('fs');
 const path=require('path');
 
-const css=fs.readFileSync(path.join(__dirname,'../app/style.css'),'utf8');
+const source=fs.readFileSync(path.join(__dirname,'../app/main-patched.js'),'utf8');
 
 test('manual accounting inputs are highlighted with a soft yellow-orange background',()=>{
-  assert.match(css,/\.salary-input\{[^}]*background:#fff3d6/i);
-  assert.match(css,/\.salary-input:focus\{[^}]*background:#ffe8ad/i);
+  assert.match(source,/background:#fff3d6/i);
+  assert.match(source,/background:#ffe8ad/i);
 });
