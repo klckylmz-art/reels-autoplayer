989c20fb9adfbf2d980dc986c4710e46b129f68a
klckylmz-art <klckylmz@gmail.com>
2026-09-18 16:07:16 +0300

    test: read manual highlight styles from group patch

--- puantaj-desktop/tests/manual-highlight.test.js
+++ puantaj-desktop/tests/manual-highlight.test.js
@@ -3,7 +3,7 @@ const assert=require('node:assert/strict');
 const fs=require('fs');
 const path=require('path');
 
-const source=fs.readFileSync(path.join(__dirname,'../app/main-patched.js'),'utf8');
+const source=fs.readFileSync(path.join(__dirname,'../app/groups-patch.js'),'utf8');
 
 test('manual accounting inputs are highlighted with a soft yellow-orange background',()=>{
   assert.match(source,/background:#fff3d6/i);
